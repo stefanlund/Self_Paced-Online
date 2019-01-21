c98c6e4e82d71cb093f248d2b51ab71f09342530
fIcklUdd5X <33080080+fIcklUdd5X@users.noreply.github.com>
2019-01-21 00:41:50 -0800

    Great exercise so far. About ready to start with indentation.

--- students/stefan_lund/Lesson_7/assignment_7/html_render.py
+++ students/stefan_lund/Lesson_7/assignment_7/html_render.py
@@ -4,9 +4,18 @@
 A class-based system for rendering html.
 """
 
+class Doctype:
+    tag = "<!DOCTYPE html>"
+
+    def render(self, out_file):
+        if self.tag == "html":
+            out_file.write(Doctype.tag)
+            out_file.write("\n")
+        Element._render(self, out_file)
+
 
 # This is the framework for the base class
-class Element:
+class Element(Doctype):
 
     def __init__(self, content=None, tag="html", **kwargs):
         self.contents = [content]
@@ -24,21 +33,27 @@ class Element:
             else:
                 self.kwargs = kwargs
 
-    def render(self, out_file):
+    def _render(self, out_file):
 
         out_file.write(self._start_tag())
 
-        for content in self.contents:
-            # out_file.write("<{}>\n".format(self.tag))
-            # try:
-            #     content.render(out_file)
-            # except AttributeError:
-            #     out_file.write(content)
-            if hasattr(content, 'tag'):
-                content.render(out_file)
-            else:
-                out_file.write(content)
-                out_file.write("\n")
+        # if self.tag == "ul":
+        #     print('self.tag == "ul", self.contents: ', self.contents, self.tag)
+        #     print(str(**self.kwargs))
+
+        if self.contents[0] is not None:    # added for class Ul
+            for content in self.contents:
+
+                # out_file.write("<{}>\n".format(self.tag))
+                # try:
+                #     content.render(out_file)
+                # except AttributeError:
+                #     out_file.write(content)
+                if hasattr(content, 'tag'):
+                    content.render(out_file)
+                else:
+                    out_file.write(content)
+                    out_file.write("\n")
         out_file.write(self._end_tag())
 
     def _start_tag(self):
@@ -60,6 +75,8 @@ class Element:
         close_tag = ">\n"
         return close_tag
 
+
+
 class Html(Element):
     tag = "html"
     def __init__(self, content=None, **kwargs):
@@ -80,6 +97,17 @@ class Head(Element):
     def __init__(self, content=None):
         super().__init__(content, Head.tag)
 
+class Ul(Element):
+    # <ul id="TheList" style="line-height:200%"> </ul>
+    tag = "ul"
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, Ul.tag, **kwargs)
+
+class Li(Element):
+    # <li style="color: red">\ncontent\n</li>\n
+    tag = "li"
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, Li.tag, **kwargs)
 
 class OneLineTag(Element):
 
@@ -88,28 +116,61 @@ class OneLineTag(Element):
         raise NotImplementedError
 
     def render(self, out_file):
-        # cont = str(*self.contents)
+    #     # cont = str(*self.contents)
 
         cont = self.contents[0]
         local_start_tag = self._start_tag()[:-1]
+    #     local_end_tag = self._end_tag()
 
         if self.tag == "a":
             local_start_tag = local_start_tag[:-1]
             cont = ">" + cont
-
+    #
         out_file.write(local_start_tag + cont + self._end_tag())
 
+    #     if self.tag == "h":
+    #         print(self.kwargs.items())
+    #         levl = str(self.kwargs["level"])
+    #         local_start_tag = local_start_tag[:2] + levl + local_start_tag[-1:]
+    #         local_end_tag = local_end_tag[:3] + levl + local_end_tag[3:]
+    #
+    #     out_file.write(local_start_tag + cont + local_end_tag)
 
 class Title(OneLineTag):
+    # <title>Python Class Sample page</title>
     tag = "title"
     def __init__(self, content=None):
         super().__init__(content, Title.tag)
 
 class A(OneLineTag):
+    # <a href="http://google.com">link</a>
     tag = "a"
     def __init__(self, link, content=None, **kwargs):
         kwargs['href'] = link
         super().__init__(content, A.tag, **kwargs)
+
+class H(OneLineTag):
+    # <h2>Python Class - Html rendering example</h2>
+    tag = "h"
+
+    def __init__(self, level, content=None, **kwargs):
+        if not (isinstance(level, int) and 0 < level < 7):
+            raise ValueError("'level' has to be an integer between 1 and 6")
+
+        self.level = level
+        self.tag = H.tag + str(self.level)
+        # self.level = level
+        # kwargs['level'] = level
+        super().__init__(content, self.tag, **kwargs)
+
+    # Not realizing the benefit of making the class attribute "tag" to an instance
+    # variable I wrote this instead. Works with the out commented lines in render method.
+    # def __init__(self, level, content=None, **kwargs):
+    #     if not (isinstance(level, int) and 0 < level < 7):
+    #         raise ValueError("'level' has to be an integer between 1 and 6")
+    #     self.level = level
+    #     kwargs['level'] = level
+    #     super().__init__(content, H.tag, **kwargs)
 
 
 class SelfClosingTag(Element):
@@ -121,13 +182,14 @@ class SelfClosingTag(Element):
         super().__init__(content, tag, **kwargs)
 
     def append(self, new_content):
-        raise TypeError("You can not add content to a SelfClosingTag")
+        raise TypeError("SelfClosingTag can not contain any content")
 
     def render(self, out_file):
         out_file.write(self._open_tag() + " /" + self._close_tag())
 
 
 class Hr(SelfClosingTag):
+    # <hr />
     tag = "hr"
     def __init__(self, content=None, **kwargs):
         super().__init__(content, Hr.tag, **kwargs)
@@ -136,3 +198,9 @@ class Br(SelfClosingTag):
     tag = "br"
     def __init__(self, content=None, **kwargs):
         super().__init__(content, Br.tag, **kwargs)
+
+class Meta(SelfClosingTag):
+    # <meta charset="UTF-8" />
+    tag = "meta"
+    def __init__(self, content=None, **kwargs):
+        super().__init__(content, Meta.tag, **kwargs)
--- students/stefan_lund/Lesson_7/assignment_7/test_html_render.py
+++ students/stefan_lund/Lesson_7/assignment_7/test_html_render.py
@@ -67,6 +67,7 @@ def test_render_element():
 
     # This uses the render_results utility above
     file_contents = render_result(e).strip()
+    print(file_contents)
 
     # making sure the content got in there.
     assert "this is some text" in file_contents
@@ -76,7 +77,8 @@ def test_render_element():
     assert file_contents.index("this is") < file_contents.index("and this")
 
     # making sure the opening and closing tags are right.
-    assert file_contents.startswith("<html>")
+    assert file_contents.startswith("<!DOCTYPE html>")
+    assert file_contents[file_contents.index(">") + 2:].startswith("<html>")
     assert file_contents.endswith("</html>")
 
     assert file_contents.count("<html>") == 1
@@ -100,6 +102,7 @@ def test_render_element2():
 
     # This uses the render_results utility above
     file_contents = render_result(e).strip()
+    print(file_contents)
 
     # making sure the content got in there.
     assert "this is some text" in file_contents
@@ -109,7 +112,8 @@ def test_render_element2():
     assert file_contents.index("this is") < file_contents.index("and this")
 
     # making sure the opening and closing tags are right.
-    assert file_contents.startswith("<html>")
+    assert file_contents.startswith("<!DOCTYPE html>")
+    assert file_contents[file_contents.index(">") + 2:].startswith("<html>")
     assert file_contents.endswith("</html>")
 
 
@@ -124,18 +128,20 @@ def test_html():
     e.append("and this is some more text")
 
     file_contents = render_result(e).strip()
+    print(file_contents)
 
     assert "this is some text" in file_contents
     assert "and this is some more text" in file_contents
-    print(file_contents)
     assert file_contents.endswith("</html>")
 
+    # assert False
 
 def test_body():
     e = Body("this is some text")
     e.append("and this is some more text")
 
     file_contents = render_result(e).strip()
+    print(file_contents)
 
     assert "this is some text" in file_contents
     assert "and this is some more text" in file_contents
@@ -149,6 +155,7 @@ def test_p():
     e.append("and this is some more text")
 
     file_contents = render_result(e).strip()
+    print(file_contents)
 
     assert "this is some text" in file_contents
     assert "and this is some more text" in file_contents
@@ -178,7 +185,6 @@ def test_sub_element():
     # but make sure the embedded element's tags get rendered!
     assert "<p>" in file_contents
     assert "</p>" in file_contents
-
 
 
 
@@ -320,6 +326,10 @@ def test_append_content_in_br():
         br = Br()
         br.append("some content")
 
+    ########
+    # Step 6
+    ########
+
 def test_anchor_class():
     e = A("http://google.com", "link to google")
     # == > <a href="http://google.com">link to google</a>
@@ -334,6 +344,90 @@ def test_anchor_class():
 
     assert file_contents.count(">") == 2
     assert file_contents.count('">') == 1
+
+    ########
+    # Step 7
+    ########
+
+def test_ul_class():
+    # <ul id="TheList" style="line-height:200%"> </ul>
+    e = Ul(id="TheList", style="line-height:200%")
+    file_contents = render_result(e).strip()
+    print("test_ul_class(), file_contents:\n", file_contents)
+
+    assert 'id="TheList"' in file_contents
+    assert 'style="line-height:200%"' in file_contents
+
+    assert file_contents.startswith("<ul ")
+    assert file_contents.endswith("</ul>")
+
+def test_li_class():
+    # <li style="color: red">\ncontent\n</li>\n
+    e = Li("this is some text")
+    e.append("and this is some more text")
+
+    file_contents = render_result(e).strip()
+
+    assert "this is some text" in file_contents
+    assert "and this is some more text" in file_contents
+
+    assert file_contents.startswith("<li>")
+    assert file_contents.endswith("</li>")
+
+def test_li_attributes():
+    e = Li(style="color: red")
+
+    file_contents = render_result(e).strip()
+    print(file_contents)  # so we can see it if the test fails
+
+    assert 'style="color: red"' in file_contents
+    # but make sure the embedded element's tags get rendered!
+    # first test the end tag is there -- same as always:
+    assert file_contents.endswith("</li>")
+
+    # but now the opening tag is far more complex
+    # but it starts the same:
+    assert file_contents.startswith("<li ")
+
+def test_header_class():
+
+    e = H(2, "The text of the header")
+    # == > <h2>The text of the header</h2>
+    file_contents = render_result(e).strip()
+    print(file_contents)  # so we can see it if the test fails
+
+    assert "The text of the header" in file_contents
+
+    assert file_contents.startswith("<h2>")
+    assert file_contents.endswith("</h2>")
+
+    assert file_contents.count(" ") == 4
+
+def test_raise_error_in_header1():
+
+    with pytest.raises(ValueError):
+        e = H(0, "Trow ValueError")
+
+def test_raise_error_in_header2():
+    with pytest.raises(ValueError):
+        e = H(7, "Trow ValueError")
+
+def test_raise_error_in_header3():
+    with pytest.raises(ValueError):
+        e = H(3.3, "Trow ValueError")
+
+def test_meta_class():
+    # <meta charset="UTF-8" />
+    """a meta class with an attribute"""
+    e = Meta(charset="UTF-8")
+    file_contents = render_result(e)
+    print(file_contents)
+    assert file_contents == '<meta charset="UTF-8" />\n'
+
+def test_meta_content():
+    with pytest.raises(TypeError):
+        e = Meta("Hello")
+
 
 # #####################
 # # indentation testing
